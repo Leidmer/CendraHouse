6f91ca2e7dc352d17027979a480e2f1b28e1b517
Sethei <sethei86239@gmail.com>
2020-05-18 14:04:23 +0200

    Documentació i Login

--- www/Cendrahous/resources/views/connect/login.blade.php
+++ www/Cendrahous/resources/views/connect/login.blade.php
@@ -1,1 +1,38 @@
-<h1>Login</h1>
+@extends('connect.master')
+
+@section('title', 'Login')
+
+@section('content')
+<div class="box box_login shadow">
+    <div class="header">
+        <a href="{{ url ('/') }}">
+            <img src="{{ url ('/static/images/logo.png') }}">
+        </a>
+    </div>
+    <div class="inside">
+    <!--Fet amb HTML collective, més fàcil i ràpid que l'html normal-->
+   {!! Form::open(['url' => '/login']) !!}
+   <label for="email">Correu Electrònic:</label>
+   <div class="input-group">
+    <div class="input-group-prepend">
+        <div class="input-group-text"><i class="far fa-envelope-open"></i></div>
+    </div>
+        {!! Form::email('email', null, ['class'=> 'form-control'])!!}
+   </div>
+
+   <label for="password" class="mtop16">Contrasenya:</label>
+   <div class="input-group">
+    <div class="input-group-prepend">
+        <div class="input-group-text"><i class="fas fa-lock"></i></div>
+    </div>
+        {!! Form::password('password', ['class'=> 'form-control'])!!}
+   </div>
+   {!! Form::submit('Entrar', ['class' => 'btn btn-success mtop16'])!!}
+   {!! Form::close() !!}
+   <div class="footer mtop16">
+    <a href="{{ url ('/register') }}">No tens un compte? Registra't</a>
+    <a href="{{ url ('/recover') }}">Recuperar Contrasenya</a>
+</div>
+</div>
+</div>
+@stop
--- www/Cendrahous/resources/views/connect/master.blade.php
+++ www/Cendrahous/resources/views/connect/master.blade.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CendraHouse - @yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--Relació absoluta i perque carregui sempre el css-->
+    <link rel="stylesheet" href="{{ url('/static/css/connect.css?v=.time()') }}">
+    <script src="https://kit.fontawesome.com/34547ed440.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    @section('content')
+    hola mundo
+    @show
 </body>
 </html>
