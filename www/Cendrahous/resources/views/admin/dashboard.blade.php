482cd4f5f0df78aa4b1f075f35e6a82990770ebb
Sethei <sethei86239@gmail.com>
2020-05-19 13:33:14 +0200

    Ja mostra usuaris en dashboard/usuaris

--- www/Cendrahous/resources/views/admin/dashboard.blade.php
+++ www/Cendrahous/resources/views/admin/dashboard.blade.php
@@ -1,1 +1,17 @@
 @extends('admin.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="container-fluid">
+    <div class="panel shadow">
+        <div class="header">
+            <h2 class="title"><i class="fas fa-home"></i> Dashboard</h2>
+        </div>
+
+        <div class="inside">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas esse et fugiat ipsam architecto amet exercitationem, voluptatem necessitatibus officia. Enim fuga officia earum laborum voluptatem non consequatur aliquid numquam!
+        </div>
+    </div>
+</div>
+@endsection
