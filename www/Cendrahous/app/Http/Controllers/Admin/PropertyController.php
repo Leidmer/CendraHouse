890a56408e59031985984a66539d33866d34c2e0
Sethei <sethei86239@gmail.com>
2020-05-20 18:32:08 +0200

    Canvis incrementals

--- www/Cendrahous/app/Http/Controllers/Admin/PropertyController.php
+++ www/Cendrahous/app/Http/Controllers/Admin/PropertyController.php
@@ -9,7 +9,7 @@ use App\Http\Models\Type, App\Http\Models\Property;
 
 
 
-use Validator, Str, Config;
+use Validator, Str, Config, Image;
 
 class PropertyController extends Controller
 {
@@ -55,12 +55,13 @@ class PropertyController extends Controller
         else:
             $path = '/'.date('Y-m-d'); // 2020-05-20 Per exemple
             $fileExt = trim($request->file('img')->getClientOriginalExtension());
-            $upload_path = Config::get('filesystems.disk.upload.root');
+            $upload_path = Config::get('filesystems.disks.uploads.root');
             //Amb slug eliminem els espais i caracters especials del nom de l'arxiu
             $name = Str::slug(str_replace($fileExt, '', $request->file('img')->getClientOriginalName()));
             //amb aquesta linia fem que si pujo un altre arxiu amb el mateix nom no el substitueix ja que li posa un nom aleatori, com a més es guarda en una carpeta diferent
             //per cada dia hi han menys probabilitats de que falli
             $filename = rand(1,999).'-'.$name.'.'.$fileExt;
+            $file_file = $upload_path.'/'.$path.'/'.$filename;
 
             $property = new Property;
             //Si la propietat està posada en 0 és un borrador i si es 1 està publicada
@@ -81,6 +82,13 @@ class PropertyController extends Controller
                 if($request->hasFile('img')):
                     //Uploads està creat a filesystems.php
                     $fl = $request->img->storeAs($path, $filename, 'uploads');
+                    $img = Image::make($file_file);
+                    //Part per si s'ha de modificar la mida
+                    $img->fit(256, 256, function($constraint){
+                        //t_ = thumbnail
+                        $constraint->upsize();
+                    });
+                    $img->save($upload_path.'/'.$path.'/t_'.$filename);
                 endif;
                 return redirect('/admin/properties')->with('message', 'S´ha guardat correctament')->with('typealert', 'success');
             endif;
